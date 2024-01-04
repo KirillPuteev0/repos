27252c8f85c50d883a6be720b0a526aaaf1bb338
KirillPuteev0 <154738859+KirillPuteev0@users.noreply.github.com>
2024-01-04 14:19:30 +0300

    Add files via upload

---
+++ git-exchange-rates-practice/git-exchange-rates-practice/Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Laboratory work #4. GIT\n";
+    cout << "Variant #4. git-exchange-rates-practice\n";
+    cout << "Author: Yana Iluchenko\n";
+    return 0;
+}
